c7098f93d8b1e83b38c7c956540177201f16b8ff
sanket099 <sinhasanket099@gmail.com>
2022-04-24 17:32:15 +0530

    ui_changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,16 @@ module.exports = {
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {
-        'body': '#17171F',
-        'selected-text': '#A3A3FF',
-        'theme': '#3F3FFF',
-        'nav': '#404053',
-        'secondary': '#9191A4',
-        'badge': '#3F3F51',
-        'input-border': '#565666',
-        'input': '#2A2A35'
-      },
+      // colors: {
+      //   'body': '#15202B',
+      //   'selected-text': '#A3A3FF',
+      //   'theme': '#1DA1F2',
+      //   'nav': '#8899A6',
+      //   'secondary': '#9191A4',
+      //   'badge': '#3F3F51',
+      //   'input-border': '#565666',
+      //   'input': '#2A2A35'
+      // },
       fontFamily: {
         'poppins': ["'Poppins'", 'sans-serif']
       }
